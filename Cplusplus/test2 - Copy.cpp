4c16fba436775cfb9684ac5be21a0a33fa388b7f
Nguyễn Mạnh Hùng <jinergenkai@gmail.com>
2022-11-08 11:02:40 +0700

    update

--- Cplusplus/test2 - Copy.cpp
+++ Cplusplus/test2 - Copy.cpp
@@ -1,62 +1,47 @@
-// CPP Program to find the number in a
-// range having maximum product of the
-// digits
-
 #include <bits/stdc++.h>
 using namespace std;
 
-// Returns the product of digits of number x
-int product(int x)
-{
-	int prod = 1;
-	while (x) {
-		prod *= (x % 10);
-		x /= 10;
-	}
-	return prod;
-}
+#define fi first
+#define se second
+#define endl '\n'
+#define int long long
+#define all(x) x.begin(),x.end()
 
-// This function returns the number having
-// maximum product of the digits
-int findNumber(string a, string b)
-{
-	// Converting both integers to strings
+const int inf = 1e9 + 7; const int N = 1000111;
+typedef pair<int, int> pii; typedef vector<int> vi; typedef long long ll;
+void print() {cout << endl;return;}
+template<typename T, typename... Args>
+void print(T a, Args... args) {cout << a << " ";print(args...);}
 
-
-	// Let the current answer be r
-	int ans = r;
-	for (int i = 0; i < b.size(); i++) {
-		if (b[i] == '0')
-			continue;
-
-		// Stores the current number having
-		// current digit one less than current
-		// digit in b
-		string curr = b;
-		curr[i] = ((curr[i] - '0') - 1) + '0';
-
-		// Replace all following digits with 9
-		// to maximise the product
-		for (int j = i + 1; j < curr.size(); j++)
-			curr[j] = '9';
-
-		// Convert string to number
-		int num = 0;
-		for (auto c : curr)
-			num = num * 10 + (c - '0');
-
-		// Check if it lies in range and its product
-		// is greater than max product
-		if (num >= l && product(ans) < product(num))
-			ans = num;
+void testcase() {
+	string s; cin >> s;
+	int n = s.size();
+	vi sumall(n, 0);
+	vi sum1(n, 0);
+	for (int i = 0; i < n; i++) {
+		sumall[i] = (i+1) * (n-i);
+		// cout << sumall[i] << " ";
+		sum1[i] = (s[i] == '1' ? sumall[i]: 0);
 	}
 
-	return ans;
+	for (int i = 1; i < n; i++) {
+		sum1[i] += sum1[i-1];
+		sumall[i] += sumall[i-1];
+	}
+
+	int mn = 0, res = sum1[n-1];
+	for (int i = 0; i < n; i++) {
+		res = max(res, sumall[i] - sum1[i] - mn + sum1[n-1]-sum1[i]);
+		mn = min(mn, sumall[i] - sum1[i] - sum1[i]);
+	}
+	cout << res << endl;
+
+	
+
 }
 
-// Driver Code
-int main()
-{
-    string a, b; cin >> a >> b;
-    findNumber(a, b);
+int32_t main() {
+int t = 1; 
+cin >> t; 
+while(t--) testcase();
 }
--- Java/CP/src/Test.java
+++ Java/CP/src/Test.java
@@ -1,34 +1,7 @@
 import java.io.*;
 import java.util.*;
 import java.lang.*;
-
-public class Test extends PrintWriter {
-    Test() { super(System.out); }
-    static Scanner sc = new Scanner(System.in);
-    final static String inputFile = "src/test.txt";
-
-    public static void main(String[] $) {
-        Test o = new Test();
-
-        boolean ok = true;
-        try {
-            File f = new File(inputFile);
-            sc = new Scanner(f);
-        } catch (Exception e) {
-            sc = new Scanner(System.in);
-            ok = false;
-        }
-        if (ok) {
-            try {
-                File f = new File(inputFile);
-                sc = new Scanner(f);
-            } catch (Exception e) {
-            }
-        }
-
-        o.main();
-        o.flush();
-    }
+public class Test extends PrintWriter { Test() { super(System.out); }static Scanner sc = new Scanner(System.in);final static String inputFile = "src/test.txt";public static void main(String[] $) {Test o = new Test();boolean ok = true;try {File f = new File(inputFile);sc = new Scanner(f);} catch (Exception e) {sc = new Scanner(System.in);ok = false;}if (ok) {try {File f = new File(inputFile);sc = new Scanner(f);} catch (Exception e) {}}o.main();o.flush();}
     void main() {
         int t = 1;
         t = sc.nextInt();
@@ -37,26 +10,6 @@ public class Test extends PrintWriter {
         }
     }
     void testcase() {
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        String s = sc.next();
 
-        int res = 0;
-        int[][] a = new int[k+1][30];
-        for (int i = 0; i < k; i++) {
-            for (int j = i; j < n; j += k) {
-                a[i][s.charAt(j)-'a']++;
-            }
-        }
-        for (int i = 0, j = k-1; i <= j; i++, j--) {
-            int maxl = 0, maxr = 0;
-            int max = 1000111000;
-            for (int l = 0; l < 26; l++) {
-                max = Math.min(max, n/k*2 - a[i][l] - a[j][l]);
-            }
-            res += max;
-            if (i == j) res -= max/2;
-        }
-        println(res);
     }
 }
